12bf97ce78a057764396355e8c83781f41c90bba
unknown <jpdonosomerlet@gmail.com>
2018-02-21 19:01:49 -0300

    se agragan monos para ver los errores, AMPD90, y que sea por lista

--- code_git/visualizacion_estimacion.py
+++ code_git/visualizacion_estimacion.py
@@ -21,8 +21,8 @@ def add_year_month_sorted(path_est):
     # se agregan las columans year y month a
     # al archivo que contienen las estimaciones
     # hechas por gp, y retorna un dataframe
-    # con los datos ordenados por f1 (que es )
-    # lo mismo que ordenar por fecha
+    # con los datos ordenados por f1 (que es
+    # lo mismo que ordenar por fecha)
     estimacion_original = pd.read_csv(path_est)
 
     # solamente se consideran las estimaciones
@@ -71,35 +71,55 @@ def sacar_repeticiones(iterable):
     return lista_sin_rep
 
 
-def plotear_f1(path_est):
-    estimacion_sorted = add_year_month_sorted(path_est)
-    YEARS = get_years(path_est)
-    dicc_anual = dict()
-    for year in YEARS:
-        df_by_year = estimacion_sorted.loc[estimacion_sorted['year'] == year]
-        meses = df_by_year['mes']
-        seen = set()
-        MESES = []  # se eliminan los meses repetidos
-        for mes in meses:
-            if mes not in seen:
-                seen.add(mes)
-                MESES.append(mes)
-        dicc_cuociente_mensual = dict()  # mes: cut_poz.mean()/cut.mean()
-        for mes in MESES:
-            cut_mes = df_by_year.loc[df_by_year['mes'] == mes]['cut']
-            cut_poz_mes = df_by_year.loc[df_by_year['mes'] == mes]['cut_poz']
-            cuociente = np.divide(cut_poz_mes.mean(), cut_mes.mean())
-            dicc_cuociente_mensual[mes] = cuociente
-        dicc_anual[year] = dicc_cuociente_mensual
+def plotear_f1(list_paths):
+    n = len(list_paths)
+    fig, axes = plt.subplots(nrows=n, ncols=1)
+    for i in range(n):
+        estimacion_sorted = add_year_month_sorted(list_paths[i])
+        YEARS = get_years(list_paths[i])
+        dicc_anual = dict()
+        for year in YEARS:
+            df_by_year = estimacion_sorted.loc[estimacion_sorted['year'] == year]
+            meses = df_by_year['mes']
+            MESES = sacar_repeticiones(meses)
+            dicc_cuociente_mensual = dict()  # mes: cut_poz.mean()/cut.mean()
+            for mes in MESES:
+                cut_mes = df_by_year.loc[df_by_year['mes'] == mes]['cut']
+                cut_poz_mes = df_by_year.loc[df_by_year['mes'] == mes]['cut_poz']
+                cuociente = np.divide(cut_poz_mes.mean(), cut_mes.mean())
+                dicc_cuociente_mensual[mes] = cuociente
+            dicc_anual[year] = dicc_cuociente_mensual
 
-    _plot_f1(dicc_anual, YEARS)
-    return dicc_anual
+        _plot_f1(dicc_anual, YEARS, i, axes)
 
 
-def plot_errores(path_est, plot_reg = False):
+def _plot_f1(dicc, years, i, ejes):
+    df_dicc = pd.DataFrame.from_dict(dicc)
+    f1 = df_dicc[years[0]]
+    for year in years[1:]:
+        f1 = pd.concat([f1, df_dicc[year]], names=['f1'])
 
-    # se grafican los errores para analisis
-    df_estimaciones = pd.read_csv(path_est)
+    f1 = f1.dropna()  # elimina todas las filas con Nan
+    f1_df = pd.DataFrame(f1.as_matrix(), index=pd.date_range('1/1/' + str(years[0]),
+                                                             periods=f1.shape[0], freq='MS'))
+    f1_df.columns = ['f1']
+    axis = ejes[i, ]
+    f1_df.plot(style='bo-', ax=axis)
+    axis.axhline(y=1.1, color='g', linestyle='-')
+    axis.axhline(y=0.9, color='g', linestyle='-')
+    return f1_df
+
+
+def plot_errores_lista(lists_path, plot_reg=False):
+    n = len(lists_path)
+    for i in range(n):
+        df_est = pd.read_csv(lists_path[i])
+        print('####################')
+        print(lists_path[i])
+        errores = plot_errores(df_est, plot_reg)
+        plt.plot(range(len(errores)), errores)
+
+def plot_errores(df_estimaciones, plot):
 
     # se filtra por puntos donde se realizó estimacion y donde existe f1
     estimacion_filtrada = df_estimaciones.loc[(df_estimaciones['f1'] > 0) &
@@ -113,12 +133,15 @@ def plot_errores(path_est, plot_reg = False):
     print('r2_score: {}'.format(r2))
     print('AMPRD90: {}'.format(AMPRD90))
 
-    if plot_reg:
+    if plot:
         plt.figure()
         plt.scatter(cut_est, cut_poz)
         plt.xlabel('Estimacion')
         plt.ylabel('Valor de pozo')
         plt.title('Estimaciones gp vs Valor de pozo')
+
+    error.sort()
+    return error
 
 
 def r2_gp_by_f1(path_est):
@@ -136,53 +159,43 @@ def r2_gp_by_f1(path_est):
     return
 
 
-def _plot_f1(dicc, years):
-    df_dicc = pd.DataFrame.from_dict(dicc)
-    f1 = df_dicc[years[0]]
-    for year in years[1:]:
-        f1 = pd.concat([f1, df_dicc[year]], names=['f1'])
-
-    f1 = f1.dropna()  # elimina todas las filas con Nan
-    f1_df = pd.DataFrame(f1.as_matrix(), index=pd.date_range('1/1/' + str(years[0]),
-                                                             periods=f1.shape[0], freq='MS'))
-    f1_df.columns = ['f1']
-    f1_df.plot(style='bo-')
-    plt.axhline(y=1.1, color='g', linestyle='-')
-    plt.axhline(y=0.9, color='g', linestyle='-')
-    return f1_df
+# def analisis_num_muestras(path_est):
 
 
 if __name__ == '__main__':
-
     path_estimacion = 'estimaciones/'
-    # # ker: Matern32(3, ARD=True)
-    # path_estimacion_5 = 'mp_test_all_5.csv'
-    # dicc_5 = dicc_error_volumen(path_estimacion_5)
 
     # ker: RBF(3, ARD=True)
+    # dist:20
     est_2 = path_estimacion + 'mp_test_all_2.csv'
-    dicc_2 = plotear_f1(est_2)
-    #
-    # # ker: sum_{i = 1}^3 RBF(x_i)
-    # path_estimacion_4 = 'mp_test_all_4.csv'
-    # dicc_4 = plotear_f1(path_estimacion_4)
 
     # ker: Matern52(3, ARD=True)
-    est_6 = path_estimacion + 'mp_test_all_6_20.csv'
-    dicc_6 = plotear_f1(est_6)  # correr con todos los holeids
+    # dist:20
+    est_6_20 = path_estimacion + 'mp_test_all_6_20.csv'
+
+    # ker: Matern52(3, ARD=True)
+    # dist:20
+    est_Mat52_20 = path_estimacion + 'mp_gp_MAt52_20.csv'
+
+    # ker: Matern52(3, ARD=True)
+    # dist:15
+    est_Mat52_15 = path_estimacion + 'mp_gp_MAt52_15.csv'
+
+    # ker: Matern52(3, ARD=True)
+    # dist:25
+    est_Mat52_25 = path_estimacion + 'mp_gp_MAt52_25.csv'
+
+    # ker: Matern52(3, ARD=True)
+    # dist:33
+    est_6_33 = path_estimacion + 'mp_test_all_6_33.csv'
 
     # se grafican los resultados de kriging
     est_ok = '../kriging/modelo_estimado_sondaje_20.csv'
-    plotear_f1(est_ok)
 
-    print('analisis gp:RBF(3, ARD)')
-    plot_errores(est_2)
-    print('########################')
-    print('analisis gp:Matern52(3, ARD)')
-    plot_errores(est_6)
-    print('########################')
-    print('analisis ok:')
-    plot_errores(est_ok)
-    print('########################')
+    paths_list = [est_2, est_Mat52_20, est_Mat52_20, est_Mat52_15, est_ok]
+    plotear_f1(paths_list)
+    plt.figure()
+    plot_errores_lista(paths_list[:4])
+
     r2_gp_by_f1(est_2)
     plt.show()
